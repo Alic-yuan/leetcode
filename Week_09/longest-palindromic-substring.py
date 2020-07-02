c473a6fc1eb4fba7cc41187d070f6bb84493a6bf
yuan <812933164@qq.com>
2020-07-02 16:01:09 +0800

    fix longest-palindromic-substring

--- Week_09/longest-palindromic-substring.py
+++ Week_09/longest-palindromic-substring.py
@@ -4,25 +4,25 @@ class Solution:
         if size < 2:
             return s
 
-        max_length = 1
-        start = 0
+        res = s[0]
+        num_min = 1
 
-        dp = [[False]*size for _ in range(size)]
+        dp = [[False] * len(s) for _ in range(len(s))]
 
-        for j in range(1, size):
+        for i in range(len(s)):
+            dp[i][i] = True
+
+        for j in range(1, len(s)):
             for i in range(j):
                 if s[i] == s[j]:
                     if j-i < 3:
                         dp[i][j] = True
                     else:
                         dp[i][j] = dp[i+1][j-1]
-                else:
-                    dp[i][j] = False
 
-                if dp[i][j]:
-                    temp = j-i+1
-                    if temp > max_length:
-                        max_length = temp
-                        start = i
+                if dp[i][j] == True:
+                    if j-i+1 > num_min:
+                        num_min = j-i+1
+                        res = s[i:j+1]
 
-        return s[start:start+max_length]
+        return res

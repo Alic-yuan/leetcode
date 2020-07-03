a607f14d0a8b6ff18f5bb3b856998ff4af5261ec
yuan <812933164@qq.com>
2020-07-03 16:25:36 +0800

    add

---
+++ Daily_practice/zhan-de-ya-ru-dan-chu-xu-lie-lcof.py
@@ -0,0 +1,11 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        i = 0
+        for j in pushed:
+            stack.append(j)
+            while stack and stack[-1] == popped[i]:
+                stack.pop()
+                i += 1
+
+        return not stack

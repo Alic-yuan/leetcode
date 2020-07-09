008f4e75fbddd9568bc3973211a0970c7ab9f6d2
yuan <812933164@qq.com>
2020-07-09 15:12:05 +0800

    update

--- Week_04/jump-game-ii.py
+++ Week_04/jump-game-ii.py
@@ -1,12 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         max_index = 0
-        step = 0
+        count = 0
         end = 0
-        for i in range(len(nums)-1):
-            if max_index >= i:
-                max_index = max(max_index, i+nums[i])
-            if i==end:
+        for i in range(len(nums) - 1):
+            max_index = max(max_index, i + nums[i])
+            if i == end:
+                count += 1
                 end = max_index
-                step +=1
-        return step
+        return count
--- Week_04/jump-game.py
+++ Week_04/jump-game.py
@@ -1,11 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        i = 0
         max_index = 0
-        for j in range(len(nums)-1):
-            if max_index >= j:
-                max_index = max(max_index, j+nums[j])
-        if max_index >= len(nums)-1:
-            return True
-        else:
-            return False
+        for i in range(len(nums) - 1):
+            if max_index >= i:
+                max_index = max(max_index, i + nums[i])
+        return max_index >= len(nums) - 1

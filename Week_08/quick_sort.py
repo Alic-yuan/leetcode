b19f66c4f50be4ae5a15d69d690dce21e2306469
yuan <812933164@qq.com>
2020-07-13 17:17:25 +0800

    update

--- Week_08/quick_sort.py
+++ Week_08/quick_sort.py
@@ -1,11 +1,11 @@
 def quick_sort(nums, left, right):
-    if left >= right:
+    if left > right:
         return
-    p = pivation(nums, left, right)
-    quick_sort(nums, left, p-1)
-    quick_sort(nums, p+1, right)
+    pivot = pivotion(nums, left, right)
+    quick_sort(nums, left, pivot-1)
+    quick_sort(nums, pivot+1, right)
 
-def pivation(nums, left, right):
+def pivotion(nums, left, right):
     mark = left
     p = nums[left]
     for i in range(left+1, right+1):
@@ -14,6 +14,7 @@ def pivation(nums, left, right):
             nums[mark], nums[i] = nums[i], nums[mark]
     nums[mark], nums[left] = nums[left], nums[mark]
     return mark
+
 
 
 nums = list([3,5,8,1,5,10,6])
